1cf7f0159ac0e0dd18119efe9207836443a6ee80
Howard Jiang <howard.yunghao.jiang@gmail.com>
2016-05-23 11:21:31 -0700

    init

---
+++ application/views/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
+        <style media="screen">
+
+        </style>
+    </head>
+    <body>
+        <div class="container">
+
+        </div>
+    </body>
+</html>
